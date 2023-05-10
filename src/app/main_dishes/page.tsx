cabcf94680d19c4709bcdd826021a0155a89d8e3
wesleywil <wesleywilsonti@gmail.com>
2023-05-10 16:35:39 -0300

    added main pages

---
+++ src/app/main_dishes/page.tsx
@@ -0,0 +1,20 @@
+import MenuItem from "@/components/menu_item/menu_item.component";
+
+export default function MainDishes() {
+  return (
+    <div className="mt-24 flex flex-col items-center justify-center font-bold text-3xl">
+      <div className="flex gap-2 items-center">
+        <h1 className="h-2 w-4 bg-black"></h1>
+        <h1>MAIN DISHES</h1>
+        <h1 className="h-2 w-4 bg-black"></h1>
+      </div>
+      <div className="mt-2 p-2 flex flex-col items-center justify-center gap-2">
+        <MenuItem />
+        <MenuItem />
+        <MenuItem />
+        <MenuItem />
+        <MenuItem />
+      </div>
+    </div>
+  );
+}
---
+++ src/components/menu_item/menu_item.component.tsx
@@ -0,0 +1,16 @@
+const MenuItem = () => {
+  return (
+    <div className="w-4/5 mx-auto flex gap-4 text-xl">
+      <div>
+        <h1>Product Name</h1>
+        <p className=" text-slate-500 text-sm">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit Lorem, ipsum
+          dolor sit amet consectetur adipisicing elit
+        </p>
+      </div>
+      <h1 className="w-48 self-center font-semibold text-red-600">$ 00.00</h1>
+    </div>
+  );
+};
+
+export default MenuItem;
