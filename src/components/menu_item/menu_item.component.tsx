3b7286140df7b8f2f32e2a32add10aaae013d17d
wesleywil <wesleywilsonti@gmail.com>
2023-05-11 13:55:28 -0300

    added product type and updated menu_item by including the product type

--- src/components/menu_item/menu_item.component.tsx
+++ src/components/menu_item/menu_item.component.tsx
@@ -1,14 +1,15 @@
-const MenuItem = () => {
+import { Product } from "@/utils/product_type/product_type";
+
+const MenuItem = (product: Product) => {
   return (
     <div className="w-4/5 mx-auto flex gap-4 text-xl">
       <div>
-        <h1>Product Name</h1>
-        <p className=" text-slate-500 text-sm">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit Lorem, ipsum
-          dolor sit amet consectetur adipisicing elit
+        <h1>{product.name}</h1>
+        <p className="w-96 h-24 text-slate-500 text-sm">
+          {product.description}
         </p>
       </div>
-      <h1 className="w-48 self-center font-semibold text-red-600">$ 00.00</h1>
+      <h1 className="w-48 font-semibold text-red-600">$ {product.price}</h1>
     </div>
   );
 };
